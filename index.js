a2e2b16eefc51253fc0aec363c34fd70a07b54ec
Rani <rani76@gmail.com>
2017-12-03 15:24:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {
+  ingredient1: "bread",
+  ingredient2: "cheese",
+  ingredient3: "tomato"
+}
+
+function updateObjectWithKeyAndValue (object, key, value) {
+ return object.assign ({}, object, {[key]: value})
+}
+
+function deleteFromObjectByKey (object, key) {
+var newObj = object.assign ({}, object)
+newObj
+delete newObj[key]
+return newObj
+}
+
+function destructivelyDeleteFromObjectByKey  (object, key) {
+delete object[key]
+return object
+}
